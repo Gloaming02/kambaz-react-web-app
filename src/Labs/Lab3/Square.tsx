b3783b5a68be88cb193c1ca519c3485f99a0fcf5
Zhengyan Hu <91642985+Gloaming02@users.noreply.github.com>
2025-02-15 23:03:46 -0800

    React is declared but not used

--- src/Labs/Lab3/Square.tsx
+++ src/Labs/Lab3/Square.tsx
@@ -1,4 +1,3 @@
-import React, { ReactNode } from "react";
 export default function Square({ children }: { children: ReactNode }) {
   const num = Number(children);
   return <span id="wd-square">{num * num}</span>;
